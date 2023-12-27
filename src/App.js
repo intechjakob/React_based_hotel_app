00e6da689566b5203e2ed0c78fd2f2e67e4093de
Yakupgenim <124262505+Yakupgenim@users.noreply.github.com>
2023-12-27 16:51:29 +0300

    Initial commit to the repo.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning react.js, APIs, and AWS
         </a>
       </header>
     </div>
